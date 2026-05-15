fde87b6e2ceea946b6843e3d9de664449adc08ca
Cephyr <178669197+cephyr-games@users.noreply.github.com>
2026-05-16 00:18:12 +0200

    fix fish history, add more nixos command abbreviations

--- modules/terminal/fish/fish.nix
+++ modules/terminal/fish/fish.nix
@@ -23,10 +23,15 @@
     {
       packages.fish = inputs.wrapper-modules.wrappers.fish.wrap {
         inherit pkgs;
+        flags."--no-config" = false;
         configFile.content = builtins.readFile ./config.fish;
         abbreviations = {
-          nixos-test = "nixos-rebuild test --flake /etc/nixos";
-          nixos-switch = "nixos-rebuild switch --flake /etc/nixos";
+          nixos-test = "sudo nixos-rebuild test --flake /etc/nixos";
+          nixos-switch = "sudo nixos-rebuild switch --flake /etc/nixos";
+          nixos-status = "sudo git -C /etc/nixos status";
+          nixos-add = "sudo git -C /etc/nixos add";
+          nixos-commit = "sudo git -C /etc/nixos commit -m";
+          nixos-edit = "sudo hx /etc/nixos";
         };
       };
     };
